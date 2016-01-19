73ffaeda553e8d7512038a074264c0ced0fac5b3
Jan Christian Bernabe <janchristianbernabe@gmail.com>
2016-01-19 10:59:33 -0600

    let's go

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var watch = require('gulp-watch');
 
 //define a task with the name of 'default'
 // and a callback to perform when the task is ran
+
+watch(['./source/*.js'], function() {
+  console.log("App has been modified; re-compiling.")
+  gulp.start('default');
+});
+
 gulp.task('default', function() {
-  console.log('I am the default task. Hear me roar');
+  return browserify('./source/app.js')
+        .transform("babelify", {presets: ["es2015", "react"]})
+        .bundle()
+        .pipe(source('build.js'))
+        .pipe(gulp.dest('./build/'))
 });
--- source/app.js
+++ source/app.js
@@ -1,5 +1,18 @@
-//so so lonely
-var app = app || {};
-app.hey = function() {
-  console.log('hey');
+// define an ES6 class called HelloWorld
+class HelloWorld {
+  constructor(name, purpose) {
+    this.attrs = {
+      name: name,
+      purpose: purpose
+    };
+  }
+  // define a toString() method on the class
+  toString() {
+    return 'Hello, world!';
+  }
 }
+// instantiate a new instance of HelloWorld
+var sample = new HelloWorld("James", "Party");
+// console.log sample's toString() method
+console.log(sample.toString());
+console.log(sample.attrs);
